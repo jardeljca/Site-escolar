2a4d1fdec9658cf803d87014a0acc2cc71e6ac4f
Jardel Queiroz <jardeljcadm@gmail.com>
2023-09-25 16:31:53 -0300

    Finalizado
    Projeto finalizado e responsivo

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -1,22 +1,37 @@
-const BotaoSociais = document.getElementById("BotaoSociais");
-const MenuSociais = document.getElementById("MenuSociais");
-const BotaoMenuLogo = document.getElementById("BotaoMenuLogo")
-const MenuLogo = document.getElementById("MenuLogo")
+function verificarLarguraEcran() {
+    const BotaoSociais = document.getElementById("BotaoSociais");
+    const MenuSociais = document.getElementById("MenuSociais");
+    const BotaoMenuLogo = document.getElementById("BotaoMenuLogo");
+    const MenuLogo = document.getElementById("MenuLogo");
+    var larguraDaJanela = window.innerWidth;
 
-BotaoSociais.addEventListener("click", () => {
-    if(MenuSociais.style.display === 'none' || MenuSociais.style.display === ''){
-        MenuSociais.style.display = 'block';
-        MenuLogo.style.display = 'none';
-    } else{
-        MenuSociais.style.display = 'none';
+    MenuLogo.style.display = 'none';
+    MenuSociais.style.display = 'none';
+    
+    if(larguraDaJanela < 560){
+        BotaoSociais.addEventListener("click", () => {
+            if(MenuSociais.style.display === 'none' || MenuSociais.style.display === ''){
+                MenuSociais.style.display = 'block';
+                MenuLogo.style.display = 'none';
+            } else{
+                MenuSociais.style.display = 'none';
+            }
+        })
+        
+        BotaoMenuLogo.addEventListener("click", () => {
+            if(MenuLogo.style.display === 'none' || MenuLogo.style.display === ''){
+                MenuLogo.style.display = 'block';
+                MenuSociais.style.display = 'none';
+            } else{
+                MenuLogo.style.display = 'none';
+            }
+        })
     }
-})
-
-BotaoMenuLogo.addEventListener("click", () => {
-    if(MenuLogo.style.display === 'none' || MenuLogo.style.display === ''){
+    else{
         MenuLogo.style.display = 'block';
-        MenuSociais.style.display = 'none';
-    } else{
-        MenuLogo.style.display = 'none';
+        MenuSociais.style.display = 'block';
     }
-})
+}
+
+window.addEventListener("load", verificarLarguraEcran);
+window.addEventListener("resize", verificarLarguraEcran);
