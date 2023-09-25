003cad30ff763747da39c7d1d5d0ccd11ef2d80d
Jardel Queiroz <jardeljcadm@gmail.com>
2023-09-25 15:27:29 -0300

    up

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -1,10 +1,22 @@
 const BotaoSociais = document.getElementById("BotaoSociais");
 const MenuSociais = document.getElementById("MenuSociais");
+const BotaoMenuLogo = document.getElementById("BotaoMenuLogo")
+const MenuLogo = document.getElementById("MenuLogo")
 
-BotaoSociais.addEventListener("click");{
+BotaoSociais.addEventListener("click", () => {
     if(MenuSociais.style.display === 'none' || MenuSociais.style.display === ''){
         MenuSociais.style.display = 'block';
+        MenuLogo.style.display = 'none';
     } else{
         MenuSociais.style.display = 'none';
     }
-}
+})
+
+BotaoMenuLogo.addEventListener("click", () => {
+    if(MenuLogo.style.display === 'none' || MenuLogo.style.display === ''){
+        MenuLogo.style.display = 'block';
+        MenuSociais.style.display = 'none';
+    } else{
+        MenuLogo.style.display = 'none';
+    }
+})
